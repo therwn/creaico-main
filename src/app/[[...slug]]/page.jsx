f0fc4d9ca1ca1b14e1ed1ae6436b641dc999dde9
Berkay Bahçeci <therwn0@gmail.com>
2026-03-26 01:45:36 +0300

    Enable edge runtime for catch-all route

--- src/app/[[...slug]]/page.jsx
+++ src/app/[[...slug]]/page.jsx
@@ -1,6 +1,8 @@
 import { headers } from 'next/headers'
 import App from '../../App'
 
+export const runtime = 'edge'
+
 export default function CatchAllPage({ params }) {
   const pathname = `/${(params.slug || []).join('/')}`.replace(/\/+$/, '') || '/'
   const host = headers().get('host')?.toLowerCase() || ''
